ea664a00c761234162001bdde2da2498a4673c85
v-weiyding <v-weiyding@microsoft.com>
2024-07-31 17:33:00 +0800

    dev

--- script/1.ps1
+++ script/1.ps1
@@ -1,3 +1,3 @@
-Write-Output ""
-Write-Output "AZD HOOK LOG : test Local file ================="
+Write-Output "AZD HOOK LOG : $($args[0])"
 Write-Output "AZD HOOK LOG : AZURE_ENV_NAME $env:AZURE_ENV_NAME "
+Write-Output "AZD HOOK LOG : test Local file ================="
--- script/1.sh
+++ script/1.sh
@@ -1,3 +1,3 @@
-echo ""
-echo "AZD HOOK LOG : Local file ================="
+echo "AZD HOOK LOG : $0 $1 "
 echo "AZD HOOK LOG : AZURE_ENV_NAME $AZURE_ENV_NAME "
+echo "AZD HOOK LOG : Local file ================="
