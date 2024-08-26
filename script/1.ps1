0e21d460d28f1ae75ec20a95cc1d93904022f5f1
v-weiyding <v-weiyding@microsoft.com>
2024-08-26 10:57:11 +0800

    sync hook3

--- script/1.ps1
+++ script/1.ps1
@@ -1,3 +1,3 @@
-Write-Output "AZD HOOK LOG : $($args[0])"
+Write-Output "\n AZD HOOK LOG : $($args[1]) =================="
 Write-Output "AZD HOOK LOG : AZURE_ENV_NAME $env:AZURE_ENV_NAME "
-Write-Output "AZD HOOK LOG : test Local file ================="
+Write-Output "AZD HOOK LOG : test $($args[0]) \n"
--- script/1.sh
+++ script/1.sh
@@ -1,3 +1,3 @@
-echo "AZD HOOK LOG : $0 $1 "
-echo "AZD HOOK LOG : AZURE_ENV_NAME $AZURE_ENV_NAME "
-echo "AZD HOOK LOG : Local file ================="
+echo  "\n AZD HOOK LOG : $1 ================="
+echo  " AZD HOOK LOG : AZURE_ENV_NAME $AZURE_ENV_NAME "
+echo  " AZD HOOK LOG : test $0 \n"
